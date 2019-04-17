8c96d2b7fbfc1b7da5413b932a4d9369dfb630d8
Marc Poppy <poppy.marc@gmail.com>
2019-04-17 04:47:10 +0000

    Automatically backed up by Learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,7 +1,7 @@
 #define method to calculate current age of person
 
 def current_age_for_birth_year(birth_year)
-  Time.now - birth_year
+  Time.now.year - birth_year
 end
 
 current_age_for_birth_year(20)
